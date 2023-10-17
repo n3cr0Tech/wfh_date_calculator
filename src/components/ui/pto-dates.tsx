55d741466a1242c82c6e0a0f8227b1df84c2d774
Jehoshua J <ztxter@gmail.com>
2023-10-17 16:12:49 -0700

    Remove button works now, added a condition that the Remove button ONLY appears when there are at least 2 rows

--- src/components/ui/pto-dates.tsx
+++ src/components/ui/pto-dates.tsx
@@ -8,14 +8,23 @@ export default function PTODates(){
         setPTOList([...ptoList, {pto: ""}]);
     };
 
+    const handlePTORemove = (index:number) => {
+        const list = [...ptoList];
+        list.splice(index, 1);
+        setPTOList(list);
+    }
+
 
     const GeneratePTOInputs = () =>{
         return ptoList.map((singlePTO, i) =>(
             <div>
-                <input name={`ptoStart_${i}`} type="text"/>               
-                <button type="button" id="remove-btn" className="btn flex-row rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2">
-                    <span>Remove</span>
-                </button>
+                <input name={`ptoStart_${i}`} type="text"/>  
+                {ptoList.length > 1 &&
+                    <button type="button" id="remove-btn" className="btn flex-row rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2"
+                        onClick={() => handlePTORemove(i)}>
+                        <span>Remove</span>
+                    </button>
+                }
                 {ptoList.length - 1 === i && ptoList.length < 10 &&
                     <button type="button" id="add-btn" className="btn flex space-x-4 rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2"
                     onClick={handlePTOAdd}
