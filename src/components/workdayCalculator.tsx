e0285aa741bdacaf988a084177898a25ad8262ed
Jehoshua J <ztxter@gmail.com>
2023-10-18 16:52:40 -0700

    Completed workdayCalculator

--- src/components/workdayCalculator.tsx
+++ src/components/workdayCalculator.tsx
@@ -10,12 +10,60 @@ export default function GetCalculatedOutputForUser(formData: FormData, ): string
     let flattenedPTODates = FlattenPTODates(formData.ptoDates.startDates, formData.ptoDates.endDates);
     let datesToGoToOffice = GetDatesToAttendOfficeWithinCycle(formData.startDate, formData.weeksInWorkCycle, flattenedPTODates);
     let totalWorkDaysInCycle = CalculateTotalWorkDays(formData.startDate, endDateInCycle);
+    let workDaysRatio = GetWorkRatio(formData.startDate, endDateInCycle);
 
+    if(ItIsPossibleToMeetAttendanceRequirement(workDaysRatio, formData.attendanceRequired)){
+        let datesToBeInTheOffice = GetDatesToAttendOfficeWithinCycle(formData.startDate, formData.weeksInWorkCycle, flattenedPTODates);
+        result = `YES: It's possible to fulfill the required office attendance of ${formData.attendanceRequired}% `;
+        result += GetSuccessOutputString(datesToBeInTheOffice);
+    }else{
+        let today = new Date();
+        result = `NO: It is too late to calculate the attendance dates based on the date today: ${today.getDate()} and the given start of the work cycled: ${formData.startDate.getDate()}\n`;
+        result += `It wont be possible to complete the attendance percentage of: ${formData.attendanceRequired}% with the given data`;
+    }
 
     return result;
 }
 
+function GetSuccessOutputString(datesToBeInOffice: Date[]): string{
+    let result = `You just need to go to the office in the following dates: `;
+    for(let i = 0; i < datesToBeInOffice.length; i++){
+        let formattedDate = FormattedDateStamp(datesToBeInOffice[i]);
+        result += `${formattedDate} \n`;
+    }
+    return result;
+}
 
+function FormattedDateStamp(dateObject: Date): string{
+    var dayTmp = String(dateObject.getDate());
+    dayTmp += "0";
+    const day = dayTmp.slice(-2);
+    
+    var monthTmp = String(dateObject.getMonth() + 1)
+    monthTmp += "0";
+    const month = monthTmp.slice(-2);
+    const year = dateObject.getFullYear();   
+
+    // const time = String(dateObject).split(' ')[4];
+    // // prints date in YYYY-MM-DD format
+    // console.log(`${year}-${month}-${day}`);
+    // // prints date & time in YYYY-MM-DD HH:MM:SS format
+    // console.log(`${year}-${month}-${day} ${time}`);
+    let result = `${year}-${month}-${day}`; 
+    return result;
+}
+
+function GetWorkRatio(startDateCycle: Date, endDateCycle: Date): number{
+    let today = new Date();
+    let diffFromTodayAndEndOfCycle = endDateCycle.getDate() - today.getDate();
+    let ratio = diffFromTodayAndEndOfCycle / (endDateCycle.getDate() - startDateCycle.getDate());
+    return ratio;
+}
+
+function ItIsPossibleToMeetAttendanceRequirement(ratio: number, attendancePercentNeeded: number): boolean{    
+    let result = ratio >= attendancePercentNeeded;
+    return result;
+}
 
 // Ex: turns start & end dates of 04/25 to 04/28 and returns Date[]:
 // [04/25, 04/26, 04/27, 04/28] (all as Date objects)
