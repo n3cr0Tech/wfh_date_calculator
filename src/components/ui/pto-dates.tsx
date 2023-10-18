408598b12083306f220037c9e4daca13b158a0a8
Jehoshua J <ztxter@gmail.com>
2023-10-17 17:07:07 -0700

    BUG FIX: turns out the fix to the textbox losing focus is by putting the code INSIDE the return(...) instead of a the function GeneratePTOInputs()

--- src/components/ui/pto-dates.tsx
+++ src/components/ui/pto-dates.tsx
@@ -23,35 +23,32 @@ export default function PTODates(){
         setPTOList(list);
     }
 
-
-    const GeneratePTOInputs = () =>{
-        return ptoList.map((ptoElement, i) =>(
-            <div key={`pto_${i}`}>
-                <input name={`ptoStart_${i}`} type="text" required className="text-black"
-                value={ptoElement.pto}
-                onChange={(e) => handlPTOElementOnChange(e, i)}
-                />  
-                {ptoList.length > 1 && (
-                    <button type="button" id="remove-btn" className="btn flex-row rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2"
-                        onClick={() => handlePTORemove(i)}>
-                        <span>Remove</span>
-                    </button>
-                )}
-                {ptoList.length - 1 === i && ptoList.length < 10 && (
-                    <button type="button" id="add-btn" className="btn flex space-x-4 rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2"
-                    onClick={handlePTOAdd}
-                    >
-                    <span>Add</span>
-                    </button>
-                )}
-            </div>
-        ));
-    }
-
     return(
         <div>
             <label>PTO Date</label>
-            <GeneratePTOInputs></GeneratePTOInputs>            
+            {
+                ptoList.map((ptoElement, i) =>(
+                    <div key={`pto_${i}`}>
+                        <input name={`ptoStart_${i}`} type="text" required className="text-black"
+                        value={ptoElement.pto}
+                        onChange={(e) => handlPTOElementOnChange(e, i)}
+                        />  
+                        {ptoList.length > 1 && (
+                            <button type="button" id="remove-btn" className="btn flex-row rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2"
+                                onClick={() => handlePTORemove(i)}>
+                                <span>Remove</span>
+                            </button>
+                        )}
+                        {ptoList.length - 1 === i && ptoList.length < 10 && (
+                            <button type="button" id="add-btn" className="btn flex space-x-4 rounded-md text-white bg-blue-600 hover:bg-blue-700 py-2 px-2"
+                            onClick={handlePTOAdd}
+                            >
+                            <span>Add</span>
+                            </button>
+                        )}
+                    </div>
+                ))
+            }
         </div>
 
     );
