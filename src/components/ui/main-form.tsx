db6bb2eb300e2cd48d1b411197619df0ba914b62
Jehoshua J <ztxter@gmail.com>
2023-10-20 13:03:24 -0700

    added labels next to textboxes

--- src/components/ui/main-form.tsx
+++ src/components/ui/main-form.tsx
@@ -65,14 +65,15 @@ export default function MainForm() {
       <div className="pt-32 pr-9 pl-9 pb-12 md:pt-25 md:pb-20 bg-slate-600 rounded-md"> 
         <div className="max-w-sm mx-auto">            
           <div className="py-5">
-              <h1 className="h1" style={{color: "white"}}>Enter Date Start & End to calculate</h1>
+              <h1 className="h1" style={{color: "white"}}>Office Attendance Calculator</h1>
           </div>
           <div className="pt-15">
             <form onSubmit={handleSubmit}>                                   
               <div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
-                </svg>                 
+                </svg>       
+                <span>Start Date of Work Cycle: </span>          
                 <DatePicker
                   id="startDateTotal"
                   className="text-black"
@@ -81,7 +82,13 @@ export default function MainForm() {
                   onChange={date => setStartDate(date)}
                   startDate={startDate}                  
                 />
+                <br/>
+                <br/>
+                <span># of weeks in work cycle: </span>          
                 <input id="weeksInEachCycle" type="text" defaultValue={12} className="form-input w-full text-black" placeholder="# of Weeks in a cycle" required/>   
+                <br/>
+                <br/>
+                <span>percent required to be in the office (e.g. '60'): </span>  
                 <input id="attendancePercentReq" type="text" defaultValue={60} className="form-input w-full text-black" placeholder="Percent of Attendance required (e.g. 60)" required/>   
               </div>
               
