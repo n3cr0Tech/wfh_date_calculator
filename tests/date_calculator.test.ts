7fea9d48c7b567f2c311af679b6a48ff885f30cc
Jehoshua J <ztxter@gmail.com>
2023-10-19 22:03:32 -0700

    Added tests

--- tests/date_calculator.test.ts
+++ tests/date_calculator.test.ts
@@ -1,5 +1,7 @@
 import { intervalToDuration } from "date-fns";
 import { CalculateTotalWorkDays, CalculateWeekendDaysAndHolidayDates, GetBankHolidaysWithinStartEndDates, GetDatesBetweenStartEndDates, GetDatesToAttendOfficeWithinCycle, GetEndDateOfWorkCycle, GetWeekendDayDates, GetWeekendDaysCount } from "../src/utils/dateCalculator";
+import GetCalculatedOutputForUser from "../src/components/workdayCalculator";
+import { FormData } from "../src/models/formData";
 
 test('Date subtractions works properly', () => {
     let today = new Date()
@@ -55,3 +57,50 @@ test('CalculateTotalWorkDays returns properly', () => {
     let actual = CalculateTotalWorkDays(startDate, endDate);
     expect(actual).toBe(expected);
 });
+
+test('GetEndDateOfWorkCycle returns properly', () => {
+    let today = new Date();
+    let startDate = new Date(`${today.getFullYear()}-01-01`);
+    let weeksInACycle = 1;
+    let actual = GetEndDateOfWorkCycle(startDate, 1);
+    let expected = new Date(`${today.getFullYear()}-01-08`);
+    expect(actual).toStrictEqual(expected);
+})
+
+test('GetDatesToAttendOfficeWithinCycle returns properly', () => {
+    let today = new Date();
+    let todayMock = new Date(`${today.getFullYear()}-01-01`);
+    let startDate = new Date(`${today.getFullYear()}-01-01`);
+
+    let pto0 = new Date(`${today.getFullYear()}-01-02`);
+    let ptoDates = [pto0];
+    let actual = GetDatesToAttendOfficeWithinCycle(todayMock, startDate, 1, ptoDates);
+    expect(actual.length).toBe(4);
+
+})
+
+test('GetCalculatedOutputForUser returns properly', () => {
+    let today = new Date();
+    let startDate = new Date(`${today.getFullYear()}-01-01`);    
+    let ptoDate0 = new Date(`${today.getFullYear()}-01-02`);
+
+    let mockFormData = createMockFormData(startDate, ptoDate0);
+    let actual = GetCalculatedOutputForUser(startDate, mockFormData);
+    expect(actual.length).toBe(4);
+});
+
+function createMockFormData(startDate: Date, ptoDate: Date): FormData{
+    
+    let mockData ={
+        startDate: startDate,
+        ptoDates: {
+            startDates: [ptoDate],
+            endDates: [ptoDate]
+        },
+        attendanceRequired: 60,
+        weeksInWorkCycle: 1
+    } as FormData;
+
+    return mockData;
+}
+
